6111bf2fa7e7b06c5722526a2a64d691e38cbb45
Dan Leman <dpleman@brandeis.edu>
2025-05-07 12:25:00 -0400

    updated male female comparison

--- ephys_behavior/DLC_huntplot_mf.m
+++ ephys_behavior/DLC_huntplot_mf.m
@@ -8,173 +8,44 @@ function DLC_huntplot(m_mat, f_mat, type, exp, hs)
 %session
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%% Compare conditions across day
-
-if type == 1
-    md1 = mean(m_mat(:,1:3),'all');
-    fd1 = mean(f_mat(:,1:3),'all');
-
-
-    figure
-    bar(0.25,mean(md1),0.4,'FaceColor', [0.07,0.62,1.00])
-    hold on,
-    bar(0.75,mean(fd1),0.4,'FaceColor', '#FF6347')
-    plot(0.15*ones(1,length(md1)), m_mat(1:3),'kx','LineWidth',4);
-    plot(0.65*ones(1,length(fd1)), f_mat(1:3),'kx','LineWidth',4);
-    errorbar(0.25,mean(md1), std(md1)/sqrt(numel(md1)), 'k', 'LineWidth',4)
-    errorbar(0.75,mean(fd1), std(fd1)/sqrt(numel(fd1)), 'k', 'LineWidth',4)
-
-    if exp == 1
-        ylabel('Time to Capture (s)')
-    elseif exp == 2
-        ylabel('Latency to Attack (s)')
-    elseif exp == 3
-        ylabel('Pursuit Duration (s)')
-    end
-
-    xlim([0 1])
-    xticks([0.25 0.75])
-    xticklabels({'Males'; 'Females'});
-    set(findall(gcf,'-property','FontSize'),'FontSize',18,'FontWeight','bold')
-    set(gca,'box','off')
-    title('Day 1')
-    hAx=gca;
-    hAx.LineWidth=3;
-    set(gca,'TickDir','out')
-
-    %Calculate Stats
-    wrs_d1 = ranksum(md1,fd1)
-
-end
-
 %% Compare Conditions by hunting session
 if type == 2 && hs <= 4
+    if exp == 1 && hs == 4
+        %% Behavior Curve
 
-    %% Compare individual sessions
-    if hs >0 && hs <=4
-        if hs < 4
-            i = hs;
-
-            mhs = m_mat(:,i);
-            fhs = f_mat(:,i);
-
-            figure
-            bar(0.25,mean(mhs),0.4,'FaceColor', [0.07,0.62,1.00])
-            hold on,
-            bar(0.75,mean(fhs),0.4,'FaceColor', [1.00,0.07,0.65])
-            plot(0.15*ones(1,length(mhs)), mhs,'kx','LineWidth',4);
-            plot(0.65*ones(1,length(fhs)), fhs,'kx','LineWidth',4);
-            errorbar(0.25,mean(mhs), std(mhs)/sqrt(numel(mhs)), 'k', 'LineWidth',4)
-            errorbar(0.75,mean(fhs), std(fhs)/sqrt(numel(fhs)), 'k', 'LineWidth',4)
-
-            if exp == 1
-                ylabel('Time to Capture (s)')
-            elseif exp == 2
-                ylabel('Latency to Attack (s)')
-            elseif exp == 3
-                ylabel('Pursuit Duration (s)')
-            end
-
-            xlim([0 1])
-            xticks([0.25 0.75])
-            xticklabels({'Male'; 'Females'});
-            set(findall(gcf,'-property','FontSize'),'FontSize',18,'FontWeight','bold')
-            set(gca,'box','off')
-            title(['Hunting Session ' num2str(i) ''])
-            hAx=gca;
-            hAx.LineWidth=3;
-            set(gca,'TickDir','out')
-
-        elseif hs == 4
-            hs2plot = 1:3;
-            for i = 1:max(hs2plot)
-                mhs = m_mat(:,i);
-                fhs = f_mat(:,i);
-                figure
-                bar(0.25,mean(mhs),0.4,'FaceColor', [0.07,0.62,1.00])
-                hold on,
-                bar(0.75,mean(fhs),0.4,'FaceColor', [1.00,0.07,0.65])
-                plot(0.15*ones(1,length(mhs)), mhs,'kx','LineWidth',4);
-                plot(0.65*ones(1,length(fhs)), fhs,'kx','LineWidth',4);
-                errorbar(0.25,mean(mhs), std(mhs)/sqrt(numel(mhs)), 'k', 'LineWidth',4)
-                errorbar(0.75,mean(fhs), std(fhs)/sqrt(numel(fhs)), 'k', 'LineWidth',4)
-
-                if exp == 1
-                    ylabel('Time to Capture (s)')
-                elseif exp == 2
-                    ylabel('Latency to Attack (s)')
-                elseif exp == 3
-                    ylabel('Pursuit Duration (s)')
-                end
-
-                xlim([0 1])
-                xticks([0.25 0.75])
-                xticklabels({'Male'; 'Females'});
-                set(findall(gcf,'-property','FontSize'),'FontSize',18,'FontWeight','bold')
-                set(gca,'box','off')
-                title(['Hunting Session ' num2str(i) ''])
-                hAx=gca;
-                hAx.LineWidth=3;
-                set(gca,'TickDir','out')
-
-            end
-
+        if size(m_mat,2) ~= size(f_mat,2)
+            disp('Conditions are not the same size!')
+            keyboard
         end
+        sess2plot = size(m_mat,2);
 
-        if exp == 1 && hs == 4
-            %% Behavior Curve
+        figure;
+        plot(1:sess2plot, mean(m_mat(:,1:sess2plot)),'Color', [0.07,0.62,1.00], 'LineWidth', 3)
+        hold on
+        plot(1:sess2plot, mean(f_mat(:,1:sess2plot)),'Color', [1.00,0.07,0.65], 'LineWidth', 3)
+        errorbar(mean(m_mat(:,1:sess2plot)),std(m_mat(:,1:sess2plot))./sqrt(numel(m_mat(:,1:sess2plot)/sess2plot)) ,'k', 'LineWidth', 2.5,'LineStyle','none')
+        errorbar(mean(f_mat(:,1:sess2plot)),std(f_mat(:,1:sess2plot))./sqrt(numel(f_mat(:,1:sess2plot)/sess2plot)) ,'k', 'LineWidth', 2.5,'LineStyle','none')
 
-            if size(m_mat,2) ~= size(f_mat,2)
-                disp('Conditions are not the same size!')
-                keyboard
-            end
-            sess2plot = size(m_mat,2);
+        xlabel('Hunting Session')
+        ylabel('Time to Capture (s)')
+        xlim([0.5 sess2plot+0.5])
+        xticks([1 2 3 4])
+        xticklabels({'1','2','3'})
+        m_max = max(m_mat(:,1:sess2plot));
+        f_max = max(f_mat(:,1:sess2plot));
+        max_mf = max([m_max,f_max]);
 
-            figure;
-            plot(1:sess2plot, mean(f_mat(:,1:sess2plot)),'Color', [1.00,0.07,0.65], 'LineWidth', 3)
-            hold on
-            plot(1:sess2plot, mean(m_mat(:,1:sess2plot)),'Color', [0.07,0.62,1.00], 'LineWidth', 3)
-            errorbar(mean(f_mat(:,1:sess2plot)),std(f_mat(:,1:sess2plot))./sqrt(numel(f_mat(:,1:sess2plot)/sess2plot)) ,'k', 'LineWidth', 2.5,'LineStyle','none')
-            errorbar(mean(m_mat(:,1:sess2plot)),std(m_mat(:,1:sess2plot))./sqrt(numel(m_mat(:,1:sess2plot)/sess2plot)) ,'k', 'LineWidth', 2.5,'LineStyle','none')
-            
-            xlabel('Hunting Session')
-            ylabel('Time to Capture (s)')
-            xlim([0.5 sess2plot+0.5])
-            xticks([1 2 3 4])
-            xticklabels({'1','2','3'})
-            m_max = max(m_mat(:,1:sess2plot));
-            f_max = max(f_mat(:,1:sess2plot));
-            max_mf = max([m_max,f_max]);
+        ylim([0 80])
+        legend('MALES (n=11)','FEMALES (n=9)')
+        title('Average Time to Capture on First Day of Hunting')
+        legend boxoff
+        set(gca,'box','off')
+        set(findall(gcf,'-property','FontSize'),'FontSize',18)
+        set(gca,'TickDir','out')
+        hAx=gca;
+        hAx.LineWidth=3;
+        set(gca,'FontSize',18,'Fontweight','bold','LineWidth',6,'box','off','tickdir','out')
 
-            ylim([0 80])
-            legend('FEMALES', 'MALES')
-            title('Average Time to Capture on First Day of Hunting')
-            legend boxoff
-            set(gca,'box','off')
-            set(findall(gcf,'-property','FontSize'),'FontSize',18)
-            set(gca,'TickDir','out')
-            hAx=gca;
-            hAx.LineWidth=3;
-            set(gca,'FontSize',18,'Fontweight','bold','LineWidth',6,'box','off','tickdir','out')
-
-        end
     end
-    
-    if exp == 1
-        disp('Stats for Time to Capture')
-    elseif exp == 2
-        disp('Stats for Latency to Attack')
-    elseif exp == 3
-        disp('Stats for Pursuit Duration')
-    end
-    
-    disp('Comparing between Conditions')
-    wrs_hs1 = ranksum(m_mat(:,1),f_mat(:,1))
-    wrs_hs2 = ranksum(m_mat(:,2),f_mat(:,2))
-    wrs_hs3 = ranksum(m_mat(:,3),f_mat(:,3))
-
-    disp('Comparing Session 1 to Session 3')
-    wsr_m1v3 = signrank(m_mat(:,1),m_mat(:,3))
-    wsr_f1v3 = signrank(f_mat(:,1),f_mat(:,3))
-    
+end
 end
